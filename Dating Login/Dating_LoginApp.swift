38c1c85458d71086a5949320f7f1528c6a08df83
Ettore Falde <ettore.falde@estudiantat.upc.edu>
2021-11-16 09:48:40 +0100

    Initial Commit

---
+++ Dating Login/Dating_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dating_LoginApp.swift
+//  Dating Login
+//
+//  Created by Ettore Falde on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Dating_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
